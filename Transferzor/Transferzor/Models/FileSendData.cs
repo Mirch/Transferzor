75a3d82c298134b0c177de71861cdb2547058332
Mircea Teodor Oprea <mirceateodor.oprea@gmail.com>
2020-04-02 22:18:35 +0300

    Add models

---
+++ Transferzor/Transferzor/Models/FileSendData.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Transferzor.Models
+{
+    public class FileSendData
+    {
+        public int Id { get; set; }
+        [DataType(DataType.EmailAddress)]
+        public string SenderEmail { get; set; }
+        [DataType(DataType.EmailAddress)]
+        public string ReceiverEmail { get; set; }
+    }
+}
---
+++ Transferzor/Transferzor/Models/FileStorageData.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Transferzor.Models
+{
+    public class FileStorageData
+    {
+        [Key]
+        public int FileSendDateId { get; set; }
+        public FileSendData FileSendData { get; set; }
+
+        [DataType(DataType.Url)]
+        public string FileUri { get; set; }
+
+    }
+}
